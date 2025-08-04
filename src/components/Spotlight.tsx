cc3e0a3b2ca41895fe06db3499559d48546fc2ad
Ram Kaushik <23071a66j2@vnrvjiet.in>
2025-08-04 23:49:57 +0530

    committed

--- src/components/Spotlight.tsx
+++ src/components/Spotlight.tsx
@@ -68,9 +68,10 @@ export default function Spotlight() {
                             className="w-full h-full z-10"
                         >
                             <TileLayer
-                                url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-                                attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+                                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                             />
+
                             {/* <TileLayer
                                 url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
                                 attribution='&copy; <a href="https://carto.com/">CARTO</a> contributors'
